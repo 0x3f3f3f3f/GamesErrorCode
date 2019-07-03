d0eda6cf5f8f9f9fea6a6502e980c8311080d350
云却 <479813005@qq.com>
2019-07-03 16:37:43 +0800

    搭建基本工程结构；

---
+++ GamesErrorCode/GamesErrorCode.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenGame
+{
+    public static class GEC
+    {
+        ///100+
+        public const int Continue = 10000000;
+        public const int SwitchingProtocols = 10100000;
+
+        ///200+
+        public const int Success = 20000000;
+        public const int Created = 20100000;
+        public const int Accepted = 20200000;
+        public const int NonAuthoritativeInformation = 20300000;
+        public const int NoContent = 20400000;
+
+
+        ///400+
+        public const int BadRequest = 40000000;
+        public const int Unauthorized = 40100000;
+        public const int PaymentRequired = 40200000;
+        public const int Forbidden = 40300000;
+        public const int NotFound = 40400000;
+        public const int MethodNotAllowd = 40500000;
+        public const int NotAcceptable = 40600000;
+        public const int ProxyAuthenticationRequired = 40700000;
+        public const int ReqeustTimeout = 40800000;
+        public const int Conflict = 40900000;
+
+
+        ///500+
+        public const int InternalServerError = 50000000;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # GamesErrorCode
-游戏通用错误状态码
+游戏通用错误状态码。  
+
+- 参考 HTTP状态码。
+- 采用8位int类型常量。  
+  例如：200 -0-11-00  
+  HTTP状态码三位-预留位一位-SubCode2位-InstanceCode2位
+- 缩写为GEC,使用时用常量 + 临时实例code。
+  ```csharp
+  public static class GEC
+  {
+      public const int Success = 20000000;
+      public const int InternalServerError = 50000000;
+  }
+
+  public class Test
+  {
+      public int SomeMethodReturnGEC()
+      {
+          return GEC.InternalServerError + 1 ;
+      }
+  }
+  ```
